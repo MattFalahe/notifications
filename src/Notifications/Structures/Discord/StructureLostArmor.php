8639e13cfe6ac0cc89308eab6eaaab65cb3f1a14
MattFalahe <47831385+MattFalahe@users.noreply.github.com>
2024-10-15 21:43:09 +0100

    Update StructureLostArmor.php

--- src/Notifications/Structures/Discord/StructureLostArmor.php
+++ src/Notifications/Structures/Discord/StructureLostArmor.php
@@ -73,6 +73,7 @@ class StructureLostArmor extends AbstractDiscordNotification
                 });
 
                 $embed->field(function (DiscordEmbedField $field) {
+                    $structure = UniverseStructure::find($this->notification->text['structureID']);
                     $type = InvType::find($this->notification->text['structureShowInfoData'][1]);
                    
                     $title = 'Structure';
