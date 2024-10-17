c74542e36ddea5046067f885a69bfb9823d8d49f
MattFalahe <47831385+MattFalahe@users.noreply.github.com>
2024-10-17 06:44:11 +0100

    StructureLostArmor.php

--- src/Notifications/Structures/Discord/StructureLostArmor.php
+++ src/Notifications/Structures/Discord/StructureLostArmor.php
@@ -74,25 +74,12 @@ class StructureLostArmor extends AbstractDiscordNotification
                 });
 
                 $embed->field(function (DiscordEmbedField $field) {
-                    // Find the structure by its ID from the notification data.
-                    // If the structure ID exists in the notification, retrieve it from the UniverseStructure model.
-                    $structure = UniverseStructure::find($this->notification->text['structureID']);
+                    $type = InvType::firstOrNew(
+                        ['typeID' => $this->notification->text['structureTypeID']],
+                        ['typeName' => trans('web::seat.unknown')]
+                    );
 
-                    // Retrieve the structure's type information using the structureShowInfoData from the notification.
-                    // The second index ([1]) contains the type ID which is used to look up the structure type from the InvType model.
-                    $type = InvType::find($this->notification->text['structureShowInfoData'][1]);
-
-                    // Initialize a default title for the structure field as 'Structure'.
-                    $title = 'Structure';
-                    
-                    // If a structure is found (i.e., it's not null), set the title to the name of the structure.
-                    if (! is_null($structure)) {
-                        $title = $structure->name;
-                    }
-
-                    // Set the field's name to the title (either 'Structure' or the structure's actual name).
-                    // Set the field's value to a zKillboard link, formatted for Discord. This uses the structure's type ID and name.
-                    $field->name($title)
+                    $field->name('Structure')
                         ->value($this->zKillBoardToDiscordLink('ship', $type->typeID, $type->typeName));
                 });
             })
